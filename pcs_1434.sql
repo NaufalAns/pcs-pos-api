0d77d32bfb4963f6e2da7e050124a85279bb3db3
NaufalAns <naufalans992@gmail.com>
2023-01-24 23:19:15 +0700

    chore(database): added some columns

--- pcs_1434.sql
+++ pcs_1434.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 14, 2022 at 05:40 AM
+-- Generation Time: Jan 24, 2023 at 04:15 PM
 -- Server version: 5.7.33
 -- PHP Version: 7.3.9
 
@@ -34,6 +34,14 @@ CREATE TABLE `admin` (
   `nama` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `email`, `password`, `nama`) VALUES
+(1, 'naufalans@gmail.com', '202cb962ac59075b964b07152d234b70', 'Naufal'),
+(4, 'admin@example.com', '202cb962ac59075b964b07152d234b70', 'Admin');
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +57,22 @@ CREATE TABLE `item_transaksi` (
   `sub_total` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `item_transaksi`
+--
+
+INSERT INTO `item_transaksi` (`id`, `transaksi_id`, `produk_id`, `qty`, `harga_saat_transaksi`, `sub_total`) VALUES
+(11, 5, 8, 1, 103000, 103000),
+(12, 6, 9, 1, 52000, 52000),
+(13, 7, 10, 1, 120000, 120000),
+(14, 8, 9, 1, 52000, 52000),
+(15, 10, 9, 1, 52000, 52000),
+(16, 11, 9, 1, 52000, 52000),
+(17, 12, 14, 1, 52000, 52000),
+(18, 12, 8, 1, 103000, 103000),
+(19, 12, 10, 1, 120000, 120000),
+(20, 13, 9, 1, 52000, 52000);
+
 -- --------------------------------------------------------
 
 --
@@ -57,11 +81,24 @@ CREATE TABLE `item_transaksi` (
 
 CREATE TABLE `produk` (
   `id` int(11) NOT NULL,
-  `admin_id` int(11) NOT NULL,
+  `admin_id` int(11) DEFAULT NULL,
+  `supplier_id` int(11) DEFAULT NULL,
   `nama` varchar(50) NOT NULL,
   `harga` int(11) NOT NULL,
-  `stok` int(11) NOT NULL
+  `stok` int(11) NOT NULL,
+  `is_supplier` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id`, `admin_id`, `supplier_id`, `nama`, `harga`, `stok`, `is_supplier`) VALUES
+(8, 4, NULL, 'Keyboard K1', 103000, 8, 0),
+(9, NULL, 1, 'Mouse K2', 52000, 16, 1),
+(10, 4, NULL, 'Mousepad K1', 120000, 3, 0),
+(14, 4, 9, 'Mouse K2', 52000, 1, 0),
+(16, 0, 0, 'Komputer', 3000000, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -72,9 +109,25 @@ CREATE TABLE `produk` (
 CREATE TABLE `transaksi` (
   `id` int(11) NOT NULL,
   `admin_id` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
   `tanggal` datetime NOT NULL,
-  `total` int(11) NOT NULL
+  `type` enum('penjualan','pembelian') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `admin_id`, `total`, `tanggal`, `type`) VALUES
+(5, 4, 103000, '2023-01-24 05:45:08', 'penjualan'),
+(6, 4, 52000, '2023-01-24 15:53:12', 'pembelian'),
+(7, 4, 120000, '2023-01-24 16:00:15', 'penjualan'),
+(8, 4, 52000, '2023-01-24 16:00:25', 'pembelian'),
+(9, 1, 1, '2023-01-24 16:02:01', 'pembelian'),
+(10, 4, 52000, '2023-01-24 16:10:33', 'pembelian'),
+(11, 4, 52000, '2023-01-24 16:11:45', 'pembelian'),
+(12, 4, 275000, '2023-01-24 16:12:33', 'penjualan'),
+(13, 4, 52000, '2023-01-24 16:12:40', 'pembelian');
 
 --
 -- Indexes for dumped tables
@@ -99,7 +152,8 @@ ALTER TABLE `item_transaksi`
 --
 ALTER TABLE `produk`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `produk_admin_id` (`admin_id`);
+  ADD KEY `produk_admin_id` (`admin_id`),
+  ADD KEY `produk_supplier_id` (`supplier_id`) USING BTREE;
 
 --
 -- Indexes for table `transaksi`
@@ -116,25 +170,25 @@ ALTER TABLE `transaksi`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `item_transaksi`
 --
 ALTER TABLE `item_transaksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
@@ -146,12 +200,6 @@ ALTER TABLE `transaksi`
 ALTER TABLE `item_transaksi`
   ADD CONSTRAINT `item_transaksi_produk_id` FOREIGN KEY (`produk_id`) REFERENCES `produk` (`id`),
   ADD CONSTRAINT `item_transaksi_transaksi_id` FOREIGN KEY (`transaksi_id`) REFERENCES `transaksi` (`id`);
-
---
--- Constraints for table `produk`
---
-ALTER TABLE `produk`
-  ADD CONSTRAINT `produk_admin_id` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`);
 
 --
 -- Constraints for table `transaksi`
